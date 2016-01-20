00d25cb1c6e8f11288f617ae1e3deda59d6b1909
Nick <adiakritos@gmail.com>
2016-01-20 14:34:50 -0800

    init commit and styles

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name basecoderApp
+ * @description
+ * # basecoderApp
+ *
+ * Main module of the application.
+ */
+
+angular.module('basecoderApp', [
+    'ngAnimate',
+    'ngMessages'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name basecoderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the basecoderApp
+ */
+angular.module('basecoderApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
